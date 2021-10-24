bf788137782d0098eb0e50a9c71710c81527e0f4
Anurodh Raina <anurodhraina@gmail.com>
2021-10-25 04:53:06 +0530

    Delete triviaapp directory

--- triviaapp/questions/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Trivia
-
-# Register your models here.
-@admin.register(Trivia)
-class TriviaModelAdmin(admin.ModelAdmin):
-	list_display=['name','first_answer','second_answer','date_created']
